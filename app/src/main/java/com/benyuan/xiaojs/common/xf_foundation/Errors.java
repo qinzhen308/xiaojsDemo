94bda193b8d8b4d4ccc5fd18ee9d09b228487d08
Maxb <maxb@jyzblm.com>
2016-11-01 17:35:09 +0800

    add api error tips

--- app/src/main/java/com/benyuan/xiaojs/common/xf_foundation/Errors.java
+++ app/src/main/java/com/benyuan/xiaojs/common/xf_foundation/Errors.java
@@ -62,6 +62,47 @@ public class Errors {
     public static final String NOT_SUPPORTED = "0x00000010";
 
 
+    //----------------------------------------------------------------------------
+    // Bad parameter.
+    public static final String BAD_PARAMETER = "0x01000001";
+
+    // The verification code is not match.
+    public static final String INVALID_CODE = "0x01000002";
+
+    // The verification code has expired.
+    public static final String BAD_CODE = "0x01000003";
+
+    // Bad request.
+    public static final String BAD_REQUEST = "0x01000004";
+
+
+    //endregion
+
+    //region Authentication & Authorization (GSS)
+
+    // Login frozen.
+    public static final String LOGIN_FROZEN = "0x10000001";
+
+    // Invalid credentials.
+    public static final String INVALID_CREDENTIAL = "0x10000002";
+
+    // Unrecognized benefit role.
+    public static final String ROLE_NOT_FOUND = "0x10000003";
+
+    // Benefit or benefit subscription not found.
+    public static final String BENEFIT_NOT_FOUND = "0x10000004";
+
+    // Unauthorized access.
+    public static final String UNAUTHORIZED = "0x11000001";
+
+    // Unauthorized access (UGC).
+    public static final String DENIED_UGC_OWNER = "0x11000002";
+
+    //endregion
+
+
+
+
     public static String getInternalErrorMessage(String errorCode){
         String errorMessage = "未知错误";
 
@@ -102,6 +143,36 @@ public class Errors {
                 break;
             case Errors.NOT_SUPPORTED:
                 errorMessage = "不支持该客户端";
+                break;
+            case Errors.BAD_PARAMETER:
+                errorMessage = "参数不正确";
+                break;
+            case Errors.INVALID_CODE:
+                errorMessage = "验证码不匹配";
+                break;
+            case Errors.BAD_CODE:
+                errorMessage = "验证码已过期";
+                break;
+            case Errors.BAD_REQUEST:
+                errorMessage = "错误请求";
+                break;
+            case Errors.LOGIN_FROZEN:
+                errorMessage = "登录被冻结";
+                break;
+            case Errors.INVALID_CREDENTIAL:
+                errorMessage = "无效的凭证";
+                break;
+            case Errors.ROLE_NOT_FOUND:
+                errorMessage = "未被认证的角色";
+                break;
+            case Errors.BENEFIT_NOT_FOUND:
+                errorMessage = "没有找到利益详情";
+                break;
+            case Errors.UNAUTHORIZED:
+                errorMessage = "未经授权的访问";
+                break;
+            case Errors.DENIED_UGC_OWNER:
+                errorMessage = "未经授权的访问(UGC)";
                 break;
             default:
                 break;
