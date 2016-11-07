a1b565cb3e70cfebbaf550c5ab0eb00433ce23ef
huangy <huangy@jyzblm.com>
2016-11-07 12:59:13 +0800

    创建课程额外信息界面

---
+++ app/src/main/java/com/benyuan/xiaojs/ui/course/CourseCreationOptionalInfoActivity.java
@@ -0,0 +1,26 @@
+package com.benyuan.xiaojs.ui.course;
+
+import com.benyuan.xiaojs.R;
+import com.benyuan.xiaojs.ui.base.BaseActivity;
+
+/*  =======================================================================================
+ *  Copyright (C) 2016 Xiaojs.cn. All rights reserved.
+ *
+ *  This computer program source code file is protected by copyright law and international
+ *  treaties. Unauthorized distribution of source code files, programs, or portion of the
+ *  package, may result in severe civil and criminal penalties, and will be prosecuted to
+ *  the maximum extent under the law.
+ *
+ *  ---------------------------------------------------------------------------------------
+ * Author:Administrator
+ * Date:2016/11/7
+ * Desc:
+ *
+ * ======================================================================================== */
+
+public class CourseCreationOptionalInfoActivity extends BaseActivity {
+    @Override
+    protected void addViewContent() {
+        setMiddleTitle(R.string.optional_info);
+    }
+}
