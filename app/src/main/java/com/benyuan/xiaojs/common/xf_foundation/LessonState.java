07665ba187d1688b8105fa1abac136e18c91e0d8
Maxb <maxb@jyzblm.com>
2016-11-09 16:11:37 +0800

    add lesson state

--- app/src/main/java/com/benyuan/xiaojs/common/xf_foundation/LessonState.java
+++ app/src/main/java/com/benyuan/xiaojs/common/xf_foundation/LessonState.java
@@ -5,4 +5,20 @@ package com.benyuan.xiaojs.common.xf_foundation;
  */
 
 public class LessonState {
+
+    public static final String DRAFT = "Draft";
+
+    public static final String PENDING_FOR_APPROVAL = "Pending for approval";
+
+    public static final String PENDING_FOR_LIVE = "Pending for live";
+
+    public static final String LIVE = "Live";
+
+    public static final String FINISHED = "Finished";
+
+    public static final String REJECTED = "Rejected";
+
+    public static final String CANCELLED = "Cancelled";
+
+    public static final String STOPPED = "Stopped";
 }
