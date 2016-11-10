61935833c9c417500c072b45acb5f40112d21b86
Maxb <maxb@jyzblm.com>
2016-11-10 20:47:08 +0800

    add getsubject demo api

---
+++ app/src/main/java/com/benyuan/xiaojs/model/GetSubjectResponse.java
@@ -0,0 +1,37 @@
+package com.benyuan.xiaojs.model;
+
+/**
+ * Created by maxiaobao on 2016/11/10.
+ */
+
+public class GetSubjectResponse {
+
+    private String id;
+    private String remarks;
+    private String name;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
