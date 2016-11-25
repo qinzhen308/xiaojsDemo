8897ff1c69e706eea47707f2c39b9b1cf22664ab
zhangh <zhangh@jyzblm.com>
2016-11-25 20:28:41 +0800

    删除无用资源

--- app/src/main/java/com/benyuan/xiaojs/ui/widget/CanInScrollviewListViewDataNotify.java
+++ app/src/main/java/com/benyuan/xiaojs/ui/widget/CanInScrollviewListViewDataNotify.java
@@ -1,16 +1,3 @@
-/*
- * Copyright (C) 2012 重庆秒银科技有限公司
- * 版权所有
- *
- * 功能描述：
- *
- * 作者：桑毅
- * 创建时间：2014年11月7日下午4:22:41
- *
- * 修改人：
- * 修改描述：
- * 修改日期
- */
 package com.benyuan.xiaojs.ui.widget;
 
 public interface CanInScrollviewListViewDataNotify {
