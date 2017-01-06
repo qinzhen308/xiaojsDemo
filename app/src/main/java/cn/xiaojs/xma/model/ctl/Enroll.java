922ce0d9c66ae3b7778c859786efd56a52f97e37
huangy <huangy@jyzblm.com>
2017-01-06 18:33:16 +0800

    报名类序列化

--- app/src/main/java/cn/xiaojs/xma/model/ctl/Enroll.java
+++ app/src/main/java/cn/xiaojs/xma/model/ctl/Enroll.java
@@ -3,12 +3,14 @@ package cn.xiaojs.xma.model.ctl;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+import java.io.Serializable;
+
 /**
  * Created by maxiaobao on 2017/1/5.
  */
 @JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Enroll {
+public class Enroll implements Serializable{
     public int max;
     public int current;
     public boolean mandatory;
