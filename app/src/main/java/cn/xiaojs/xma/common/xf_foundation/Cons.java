0380f2534ceeaa6d38578d99af159dfb5955ff8c
Maxb <maxb@jyzblm.com>
2017-02-06 18:42:23 +0800

    Add colla api impl

---
+++ app/src/main/java/cn/xiaojs/xma/common/xf_foundation/Cons.java
@@ -0,0 +1,8 @@
+package cn.xiaojs.xma.common.xf_foundation;
+
+/**
+ * Created by maxiaobao on 2017/2/6.
+ */
+
+public class Cons {
+}
---
+++ app/src/main/java/cn/xiaojs/xma/common/xf_foundation/Constants.java
@@ -0,0 +1,9 @@
+package cn.xiaojs.xma.common.xf_foundation;
+
+/**
+ * Created by maxiaobao on 2017/2/6.
+ */
+
+public class Constants {
+
+}
