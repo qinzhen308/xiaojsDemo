a1f6d428aef79ead7e6eff07435580ebeab0551d
Maxb <maxb@jyzblm.com>
2017-02-06 12:26:14 +0800

    Add live sessions api impl

--- app/src/main/java/cn/xiaojs/xma/common/xf_foundation/constants.java
+++ app/src/main/java/cn/xiaojs/xma/common/xf_foundation/constants.java
@@ -1,9 +1,10 @@
 package cn.xiaojs.xma.common.xf_foundation;
 
 /**
- * Created by maxiaobao on 2017/2/4.
+ * Created by maxiaobao on 2017/2/6.
  */
 
 public class Constants {
+
     public static final String XCFSUrl= "ok50ctnzl.bkt.clouddn.com";
 }
