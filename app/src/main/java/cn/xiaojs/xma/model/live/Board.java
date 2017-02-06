d5c927706fb6e03a74b82c01b9c95c62efe1a864
Maxb <maxb@jyzblm.com>
2017-02-06 11:47:33 +0800

    Add live sessions api impl

--- app/src/main/java/cn/xiaojs/xma/model/live/Board.java
+++ app/src/main/java/cn/xiaojs/xma/model/live/Board.java
@@ -1,12 +1,16 @@
 package cn.xiaojs.xma.model.live;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import cn.xiaojs.xma.common.xf_foundation.schemas.Live;
 import cn.xiaojs.xma.model.social.Dimension;
 
 /**
  * Created by maxiaobao on 2017/2/6.
  */
-
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Board {
     public String title;
     public long sort = System.currentTimeMillis();
