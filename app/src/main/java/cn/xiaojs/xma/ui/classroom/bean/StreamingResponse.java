84ebb57fa1fe5e9401449d8f0ea4ef1e7534f544
huangy <huangy@jyzblm.com>
2017-02-13 17:49:31 +0800

    教室，推流流程调整

---
+++ app/src/main/java/cn/xiaojs/xma/ui/classroom/bean/StreamingResponse.java
@@ -0,0 +1,21 @@
+package cn.xiaojs.xma.ui.classroom.bean;
+/*  =======================================================================================
+ *  Copyright (C) 2016 Xiaojs.cn. All rights reserved.
+ *
+ *  This computer program source code file is protected by copyright law and international
+ *  treaties. Unauthorized distribution of source code files, programs, or portion of the
+ *  package, may result in severe civil and criminal penalties, and will be prosecuted to
+ *  the maximum extent under the law.
+ *
+ *  ---------------------------------------------------------------------------------------
+ * Author:huangyong
+ * Date:2017/2/13
+ * Desc: 推流started和stopped
+ *
+ * ======================================================================================== */
+
+public class StreamingResponse {
+    public boolean result;
+    public String ec;
+    public Object details;
+}
