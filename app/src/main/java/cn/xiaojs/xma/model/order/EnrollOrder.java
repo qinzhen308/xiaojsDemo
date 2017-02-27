fb98c70a6ac0e084600d58d9f2f71982d18e0248
Maxb <maxb@jyzblm.com>
2017-02-27 19:20:01 +0800

    fixed bugs when contact's circle avatar

--- app/src/main/java/cn/xiaojs/xma/model/order/EnrollOrder.java
+++ app/src/main/java/cn/xiaojs/xma/model/order/EnrollOrder.java
@@ -1,14 +1,19 @@
 package cn.xiaojs.xma.model.order;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 /**
  * Created by maxiaobao on 2017/2/27.
  */
-
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_EMPTY)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class EnrollOrder {
 
     public int no;
     public float amount;
 
+    public PaymentOrder items;
     public String promotion;
     public int quantity;
     public String body;
