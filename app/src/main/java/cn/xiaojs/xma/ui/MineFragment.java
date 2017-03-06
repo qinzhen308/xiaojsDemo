7ca996d26ed713ab11ce9bf74dff379d27130ebc
huangy <huangy@jyzblm.com>
2017-03-06 12:31:52 +0800

    教室，我的界面头像加载。

--- app/src/main/java/cn/xiaojs/xma/ui/MineFragment.java
+++ app/src/main/java/cn/xiaojs/xma/ui/MineFragment.java
@@ -19,18 +19,12 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.animation.GlideAnimation;
-import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
-import com.bumptech.glide.signature.StringSignature;
 
 import butterknife.BindView;
 import butterknife.OnClick;
@@ -50,13 +44,12 @@ import cn.xiaojs.xma.ui.lesson.TeachingSubjectActivity;
 import cn.xiaojs.xma.ui.mine.MyOrderActivity;
 import cn.xiaojs.xma.ui.mine.ProfileActivity;
 import cn.xiaojs.xma.ui.mine.SettingsActivity;
-import cn.xiaojs.xma.ui.mine.TeachingAbilityActivity;
 import cn.xiaojs.xma.ui.personal.PersonHomeActivity;
 import cn.xiaojs.xma.ui.personal.PersonalBusiness;
 import cn.xiaojs.xma.ui.widget.EvaluationStar;
 import cn.xiaojs.xma.ui.widget.IconTextView;
 import cn.xiaojs.xma.ui.widget.PortraitView;
-import cn.xiaojs.xma.util.DeviceUtil;
+import cn.xiaojs.xma.util.ExpandGlide;
 import cn.xiaojs.xma.util.FastBlur;
 
 public class MineFragment extends BaseFragment {
@@ -209,35 +202,20 @@ public class MineFragment extends BaseFragment {
         mPortraitView.setBorderColor(getResources().getColor(R.color.round_img_border));
         mPortraitView.setBorderWidth(R.dimen.px2);
 
-        //TODO test
-//        if (true) {
-//            mPortraitView.setImageResource(R.drawable.mine_portrait);
-//            Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.mine_portrait);
-//            setupBlurPortraitView(bmp);
-//            return;
-//        }
-
         //set avatar
         if (basic != null) {
-            String s = cn.xiaojs.xma.common.xf_foundation.schemas.Account.getAvatar(AccountDataManager.getAccountID(mContext),300);
-
-            Glide.with(mContext).load(s)
-                    .signature(new StringSignature(DeviceUtil.getSignature()))
+            String u = cn.xiaojs.xma.common.xf_foundation.schemas.Account.getAvatar(AccountDataManager.getAccountID(mContext),300);
+            ExpandGlide expandGlide = new ExpandGlide();
+            expandGlide.with(mContext)
+                    .load(Uri.parse(u))
+                    .placeHolder(R.drawable.default_avatar)
                     .error(R.drawable.default_avatar)
-                    .into(new GlideDrawableImageViewTarget(mPortraitView) {
+                    .into(mPortraitView, new ExpandGlide.OnBitmapLoaded() {
                         @Override
-                        public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> animation) {
-                            super.onResourceReady(resource, animation);
-                            if (resource instanceof GlideBitmapDrawable) {
-                                Bitmap bmp = ((GlideBitmapDrawable) resource).getBitmap();
+                        public void onLoaded(Bitmap bmp) {
+                            if (bmp != null) {
                                 setupBlurPortraitView(bmp);
                             }
-                        }
-
-                        @Override
-                        public void onLoadFailed(Exception e, Drawable errorDrawable) {
-                            super.onLoadFailed(e, errorDrawable);
-                            setDefaultPortrait();
                         }
                     });
         } else {
