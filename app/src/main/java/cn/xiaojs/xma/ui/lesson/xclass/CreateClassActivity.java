8ac78bb9d0b6b1d78c355cd5e6ae3c19e6c1ff3f
Maxb <maxb@jyzblm.com>
2017-05-17 15:18:08 +0800

    support create class

---
+++ app/src/main/java/cn/xiaojs/xma/ui/lesson/xclass/CreateClassActivity.java
@@ -0,0 +1,16 @@
+package cn.xiaojs.xma.ui.lesson.xclass;
+
+import cn.xiaojs.xma.R;
+import cn.xiaojs.xma.ui.base.BaseActivity;
+
+/**
+ * Created by maxiaobao on 2017/5/17.
+ */
+
+public class CreateClassActivity extends BaseActivity {
+
+    @Override
+    protected void addViewContent() {
+        setMiddleTitle(getString(R.string.create_class));
+    }
+}
