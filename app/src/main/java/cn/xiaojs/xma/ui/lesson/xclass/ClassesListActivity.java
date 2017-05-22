a3c240b3a403e5ca5763bf6ca26b7bd0268fb8eb
Maxb <maxb@jyzblm.com>
2017-05-22 18:27:13 +0800

    Signed-off-by: Maxb <maxb@jyzblm.com>

---
+++ app/src/main/java/cn/xiaojs/xma/ui/lesson/xclass/ClassesListActivity.java
@@ -0,0 +1,20 @@
+package cn.xiaojs.xma.ui.lesson.xclass;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by maxiaobao on 2017/5/22.
+ */
+
+public class ClassesListActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+}
