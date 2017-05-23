a4899eb71e64afa303a2a5ef61ca0db05e212323
Maxb <maxb@jyzblm.com>
2017-05-23 16:49:25 +0800

    Signed-off-by: Maxb <maxb@jyzblm.com>

---
+++ app/src/main/java/cn/xiaojs/xma/ui/lesson/xclass/view/ClassLessonView.java
@@ -0,0 +1,20 @@
+package cn.xiaojs.xma.ui.lesson.xclass.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by maxiaobao on 2017/5/23.
+ */
+
+public class ClassLessonView extends RelativeLayout {
+
+    public ClassLessonView(Context context) {
+        super(context);
+    }
+
+    public ClassLessonView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
