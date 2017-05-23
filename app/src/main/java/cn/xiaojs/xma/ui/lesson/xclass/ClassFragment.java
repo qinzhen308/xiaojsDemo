52c92e18091f99b1e7bf3356ecc0b5e8a3814938
Maxb <maxb@jyzblm.com>
2017-05-23 11:58:43 +0800

    Signed-off-by: Maxb <maxb@jyzblm.com>

--- app/src/main/java/cn/xiaojs/xma/ui/lesson/xclass/ClassFragment.java
+++ app/src/main/java/cn/xiaojs/xma/ui/lesson/xclass/ClassFragment.java
@@ -1,11 +1,24 @@
 package cn.xiaojs.xma.ui.lesson.xclass;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by maxiaobao on 2017/5/22.
  */
 
 public class ClassFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
