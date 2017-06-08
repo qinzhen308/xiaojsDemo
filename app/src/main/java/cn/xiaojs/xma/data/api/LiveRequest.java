cd5865fc5773f13999e525b2ba82c3787a18bb46
Maxb <maxb@jyzblm.com>
2017-06-08 11:34:26 +0800

    Signed-off-by: Maxb <maxb@jyzblm.com>

--- app/src/main/java/cn/xiaojs/xma/data/api/LiveRequest.java
+++ app/src/main/java/cn/xiaojs/xma/data/api/LiveRequest.java
@@ -4,6 +4,8 @@ import android.content.Context;
 
 import com.orhanobut.logger.Logger;
 
+import java.util.List;
+
 import cn.xiaojs.xma.XiaojsConfig;
 import cn.xiaojs.xma.data.api.service.APIServiceCallback;
 import cn.xiaojs.xma.data.api.service.APIType;
@@ -19,6 +21,7 @@ import cn.xiaojs.xma.model.live.ClassResponse;
 import cn.xiaojs.xma.model.live.CtlSession;
 import cn.xiaojs.xma.model.live.LiveCollection;
 import cn.xiaojs.xma.model.live.LiveCriteria;
+import cn.xiaojs.xma.model.live.LiveSchedule;
 import cn.xiaojs.xma.model.live.TalkItem;
 import cn.xiaojs.xma.model.live.Ticket;
 import cn.xiaojs.xma.model.Pagination;
@@ -131,7 +134,7 @@ public class LiveRequest extends ServiceRequest{
             Logger.json(criteriaJsonstr);
             Logger.json(paginationJsonstr);
         }
-        Call<ResponseBody> call = getLiveService().getLiveSchedule(ticket,
+        Call<List<LiveSchedule>> call = getLiveService().getLiveSchedule(ticket,
                 criteriaJsonstr,
                 paginationJsonstr);
 
