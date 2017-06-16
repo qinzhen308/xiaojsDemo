c3c15e733cccea1bfe25bdded3cadd1386d0cf78
Maxb <maxb@jyzblm.com>
2017-06-16 10:43:25 +0800

    Signed-off-by: Maxb <maxb@jyzblm.com>

--- app/src/main/java/cn/xiaojs/xma/data/api/socket/SocketManager.java
+++ app/src/main/java/cn/xiaojs/xma/data/api/socket/SocketManager.java
@@ -1,10 +1,94 @@
 package cn.xiaojs.xma.data.api.socket;
 
+import android.content.Context;
+
+import java.util.concurrent.TimeUnit;
+
+import cn.xiaojs.xma.XiaojsConfig;
+import cn.xiaojs.xma.data.api.ApiManager;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okio.ByteString;
+
 /**
  * Created by maxiaobao on 2017/6/7.
  */
 
 public class SocketManager {
+
+
+    public void init() {
+
+        Request request = new Request.Builder().url("").build();
+
+        OkHttpClient okHttpClient = createOkhttp();
+
+        okHttpClient.newWebSocket(request, new WebSocketListener() {
+            @Override
+            public void onOpen(WebSocket webSocket, Response response) {
+                super.onOpen(webSocket, response);
+            }
+
+            @Override
+            public void onMessage(WebSocket webSocket, String text) {
+                super.onMessage(webSocket, text);
+            }
+
+            @Override
+            public void onMessage(WebSocket webSocket, ByteString bytes) {
+                super.onMessage(webSocket, bytes);
+            }
+
+            @Override
+            public void onClosing(WebSocket webSocket, int code, String reason) {
+                super.onClosing(webSocket, code, reason);
+            }
+
+            @Override
+            public void onClosed(WebSocket webSocket, int code, String reason) {
+                super.onClosed(webSocket, code, reason);
+            }
+
+            @Override
+            public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+                super.onFailure(webSocket, t, response);
+            }
+        });
+
+    }
+
+    private OkHttpClient createOkhttp() {
+
+        HttpLoggingInterceptor logInterceptor = new HttpLoggingInterceptor();
+        if (XiaojsConfig.DEBUG) {
+            logInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        } else {
+
+            logInterceptor.setLevel(HttpLoggingInterceptor.Level.NONE);
+        }
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+                .addInterceptor(logInterceptor)
+                .addNetworkInterceptor(logInterceptor)
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS);
+
+        return builder.build();
+
+    }
+
+    private String getSocketUrl(Context context, String ticket) {
+        return new StringBuilder(ApiManager.getXLSUrl(context))
+                .append("/")
+                .append(ticket)
+                .toString();
+    }
+
 
 
 }
