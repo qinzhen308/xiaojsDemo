05f6948f5339365fbd1a0b66e29ad20db388b84c
Maxb <maxb@jyzblm.com>
2017-10-18 09:34:28 +0800

    Signed-off-by: Maxb <maxb@jyzblm.com>

--- app/src/main/java/cn/xiaojs/xma/ui/classroom2/IdleFragment.java
+++ app/src/main/java/cn/xiaojs/xma/ui/classroom2/IdleFragment.java
@@ -6,10 +6,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.orhanobut.logger.Logger;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import cn.xiaojs.xma.R;
+import cn.xiaojs.xma.XiaojsConfig;
+import cn.xiaojs.xma.common.xf_foundation.Su;
+import cn.xiaojs.xma.common.xf_foundation.schemas.Live;
+import cn.xiaojs.xma.data.api.service.APIServiceCallback;
+import cn.xiaojs.xma.model.live.ClassResponse;
+import cn.xiaojs.xma.model.socket.room.EventReceived;
 import cn.xiaojs.xma.ui.classroom2.base.MovieFragment;
+import cn.xiaojs.xma.ui.classroom2.core.EventListener;
+import io.reactivex.functions.Consumer;
 
 /**
  * Created by maxiaobao on 2017/9/25.
@@ -20,6 +31,8 @@ public class IdleFragment extends MovieFragment {
 
     @BindView(R.id.p_bottom_class_name)
     TextView classNameView;
+
+    private EventListener.ELIdle idleObserver;
 
 
     @Nullable
@@ -36,9 +49,16 @@ public class IdleFragment extends MovieFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        controlLand.setVisibility(View.GONE);
-        pBottomClassnameView.setText(classroomEngine.getRoomTitle());
-        startOrStopLiveView.setText("开始直播");
+        initControlPanel();
+        idleObserver = classroomEngine.observerIdle(receivedConsumer);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (idleObserver != null) {
+            idleObserver.dispose();
+        }
     }
 
 
@@ -69,7 +89,18 @@ public class IdleFragment extends MovieFragment {
 
     @Override
     public void onStartLiveClick(View view) {
-        requestLive();
+        if (classroomEngine.canIndividualByState()) {
+            requestLive();
+        } else {
+
+            if (classroomEngine.getLiveMode() == Live.ClassroomMode.TEACHING) {
+
+                //开始上课
+
+
+            }
+        }
+
     }
 
     @Override
@@ -78,6 +109,73 @@ public class IdleFragment extends MovieFragment {
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
+    // 操作面板
+    //
+
+    private void initControlPanel() {
+
+        controlLand.setVisibility(View.GONE);
+        pBottomClassnameView.setText(classroomEngine.getRoomTitle());
+
+        if (classroomEngine.canIndividualByState()) {
+            startOrStopLiveView.setText("开始直播");
+            startOrStopLiveView.setVisibility(View.VISIBLE);
+        }else {
+
+            if (classroomEngine.getLiveMode() == Live.ClassroomMode.TEACHING) {
+                startOrStopLiveView.setText("开始上课");
+                startOrStopLiveView.setVisibility(View.VISIBLE);
+            }else {
+                startOrStopLiveView.setVisibility(View.GONE);
+            }
+
+
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // 开始上课
+    //
+
+    private void requestBeginClass() {
+        showProgress(true);
+        classroomEngine.beginClass(classroomEngine.getTicket(), new APIServiceCallback<ClassResponse>() {
+            @Override
+            public void onSuccess(ClassResponse object) {
+                cancelProgress();
+            }
+
+            @Override
+            public void onFailure(String errorCode, String errorMessage) {
+                cancelProgress();
+            }
+        });
+    }
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // 事件监听
+    //
+
+    private Consumer<EventReceived> receivedConsumer = new Consumer<EventReceived>() {
+        @Override
+        public void accept(EventReceived eventReceived) throws Exception {
+
+            if (XiaojsConfig.DEBUG) {
+                Logger.d("receivedConsumer .....");
+            }
+
+            switch (eventReceived.eventType) {
+                case Su.EventType.STREAMING_STARTED:
+                    break;
+                case Su.EventType.SYNC_CLASS_STATE:
+                    break;
+                case Su.EventType.SYNC_STATE:
+
+                    break;
+            }
+        }
+    };
 
 
 
